73a8e4f9ca4ab0b10cdecf305d9ccfaa4a6ef336
Nexusrisen <bdkohn91@gmail.com>
2018-10-06 17:40:26 -0400

    Added SASPJob Vehicles, Organized

--- FXServer/resources/[esx]/esx_saspjob/__resource.lua
+++ FXServer/resources/[esx]/esx_saspjob/__resource.lua
@@ -30,10 +30,21 @@ client_scripts {
 	'locales/es.lua',
 	'locales/sv.lua',
 	'config.lua',
-	'client/main.lua'
+	'client/main.lua',
+	'data/vehicle_names.lua'
 }
 
 dependencies {
 	'es_extended',
 	'esx_billing'
 }
+
+files {
+	'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carvariations.meta'
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'

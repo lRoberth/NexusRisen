82180b912ff26e952f916fed2e3cdf25d5425df6
Nexusrisen <bdkohn91@gmail.com>
2018-10-07 17:23:27 -0400

    Fixed PoliceJob Locals

--- FXServer/resources/[esx]/esx_policejob/__resource.lua
+++ FXServer/resources/[esx]/esx_policejob/__resource.lua
@@ -9,28 +9,14 @@ this_is_a_map 'yes'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua'
 }

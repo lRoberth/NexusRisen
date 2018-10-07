f0d784b515dfa48d55305d9265a75e0c5d67334e
Nexusrisen <bdkohn91@gmail.com>
2018-10-07 17:25:11 -0400

    Fixed SASP Locals

--- FXServer/resources/[esx]/esx_saspjob/__resource.lua
+++ FXServer/resources/[esx]/esx_saspjob/__resource.lua
@@ -7,28 +7,14 @@ version '1.2.0'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua',
 	'data/vehicle_names.lua'
